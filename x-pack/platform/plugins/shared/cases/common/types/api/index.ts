a3da002375138268e4792eeca922d82f04866791
Dominique Belcher <dominique.clarke@elastic.co>
2025-08-26 10:45:44 -0400

    export types

--- x-pack/platform/plugins/shared/cases/common/types/api/index.ts
+++ x-pack/platform/plugins/shared/cases/common/types/api/index.ts
@@ -18,6 +18,7 @@ export * from './attachment/latest';
 export * from './metrics/latest';
 export * from './custom_field/latest';
 export * from './observable/latest';
+export * from './suggestions/latest';
 
 // V1
 export * as configureApiV1 from './configure/v1';
@@ -32,3 +33,4 @@ export * as attachmentApiV1 from './attachment/v1';
 export * as metricsApiV1 from './metrics/v1';
 export * as customFieldsApiV1 from './custom_field/v1';
 export * as observableApiV1 from './observable/v1';
+export * as suggestionsApiV1 from './suggestions/v1';
